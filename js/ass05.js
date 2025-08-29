896e6d8f8a53342c3a708cd45d9aa29ce2c6cea1
FARIYA <fariyaa2001@gmail.com>
2025-08-29 20:01:03 +0600

    added the updated version

---
+++ js/ass05.js
@@ -0,0 +1,49 @@
+let coins = 100;
+  let copyCount = 0;
+  let favCount = 0;
+
+  function updateUI() {
+    document.getElementById("coinCount").innerText = coins;
+    document.getElementById("copyCount").innerText = copyCount;
+    document.getElementById("favCount").innerText = favCount;
+  }
+
+  function copyNumber(number) {
+    navigator.clipboard.writeText(number);
+    copyCount++;
+    updateUI();
+    alert("Number " + number + " copied!");
+  }
+
+  function makeCall(service, number) {
+    if (coins < 20) {
+      alert("Not enough coins to make a call!");
+      return;
+    }
+    coins -= 20;
+    updateUI();
+    alert("Calling " + service + " (" + number + ")");
+    let now = new Date();
+    let time = now.toLocaleTimeString();
+    let historyItem = `<div class="border-b pb-1"><strong>${service}</strong> (${number}) <br><span class="text-xs text-gray-500">${time}</span></div>`;
+    document.getElementById("historyList").innerHTML += historyItem;
+  }
+
+  function clearHistory() {
+    document.getElementById("historyList").innerHTML = "";
+  }
+
+  function toggleFav(el) {
+    if (el.classList.contains("far")) {
+      el.classList.remove("far");
+      el.classList.add("fas", "text-red-500");
+      favCount++;
+    } else {
+      el.classList.remove("fas", "text-red-500");
+      el.classList.add("far");
+      favCount--;
+    }
+    updateUI();
+  }
+
+  updateUI();
