4fe6f957561b9761aeb425eda60af7e1c0df9371
FARIYA <fariyaa2001@gmail.com>
2025-08-29 22:39:58 +0600

    added thecall history

--- js/ass05.js
+++ js/ass05.js
@@ -1,24 +1,24 @@
 let coins = 100;
-  let copyCount = 0;
-  let favCount = 0;
+let copyCount = 0;
+let favCount = 0;
 
-  function updateUI() {
+function updateUI() {
     document.getElementById("coinCount").innerText = coins;
     document.getElementById("copyCount").innerText = copyCount;
     document.getElementById("favCount").innerText = favCount;
-  }
+}
 
-  function copyNumber(number) {
+function copyNumber(number) {
     navigator.clipboard.writeText(number);
     copyCount++;
     updateUI();
     alert("Number " + number + " copied!");
-  }
+}
 
-  function makeCall(service, number) {
+function makeCall(service, number) {
     if (coins < 20) {
-      alert("Not enough coins to make a call!");
-      return;
+        alert("Not enough coins to make a call!");
+        return;
     }
     coins -= 20;
     updateUI();
@@ -27,23 +27,23 @@ let coins = 100;
     let time = now.toLocaleTimeString();
     let historyItem = `<div class="border-b pb-1"><strong>${service}</strong> (${number}) <br><span class="text-xs text-gray-500">${time}</span></div>`;
     document.getElementById("historyList").innerHTML += historyItem;
-  }
+}
 
-  function clearHistory() {
+function clearHistory() {
     document.getElementById("historyList").innerHTML = "";
-  }
+}
 
-  function toggleFav(el) {
+function toggleFav(el) {
     if (el.classList.contains("far")) {
-      el.classList.remove("far");
-      el.classList.add("fas", "text-red-500");
-      favCount++;
+        el.classList.remove("far");
+        el.classList.add("fas", "text-red-500");
+        favCount++;
     } else {
-      el.classList.remove("fas", "text-red-500");
-      el.classList.add("far");
-      favCount--;
+        el.classList.remove("fas", "text-red-500");
+        el.classList.add("far");
+        favCount--;
     }
     updateUI();
-  }
+}
 
-  updateUI();
+updateUI();
